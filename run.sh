26346dfd0770d50c2d701b750aba08d1973f0aab
Colin Rees <cmrees@gmail.com>
2018-11-14 18:01:02 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 # Program Execution Script.
-#infile='./input/h1b_input.csv'
+infile='./input/h1b_input.csv'
 #infile='./input/H1B_FY_2016.csv'
 outfile1='./output/top_10_occupations.txt'
 outfile2='./output/top_10_states.txt'
 
-for infile in `ls input/*.csv`
-do
-    python ./src/h1b_certification.py -i $infile -o1 $outfile1 -o2 $outfile2
-done
+#for infile in `ls input/*.csv`
+#do
+
+
+python ./src/h1b_certification.py -i $infile -o1 $outfile1 -o2 $outfile2
+#done
